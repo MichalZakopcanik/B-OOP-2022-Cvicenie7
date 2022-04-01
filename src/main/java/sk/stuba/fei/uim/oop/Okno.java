078c3e6ae78df3c8cb741299abea2c87f43ec64f
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-04-01 23:06:02 +0200

    My panel with color

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -7,39 +7,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
 
-public class Okno extends JFrame implements ActionListener {
+public class Okno extends JFrame {
     public Okno() throws HeadlessException {
         super("Okno");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(400,400);
+        this.setSize(400, 400);
 
-        this.setLayout(new BorderLayout());
-
-        JPanel menu = new JPanel();
-        menu.setLayout(new GridLayout(1,2));
-        menu.setBackground(Color.CYAN);
-        JLabel label = new JLabel("Tu je text vlavo");
-        JLabel label2 = new JLabel("Tu je text vpravo");
-        menu.add(label);
-        menu.add(label2);
-
-        JPanel klavesnica = new JPanel();
-        klavesnica.setLayout(new GridLayout(3,3));
-
-        for (int i = 1; i < 10; i++){
-            JButton b = new JButton(String.valueOf(i));
-            klavesnica.add(b);
-            b.addActionListener(this);
-        }
-
-        this.add(menu, BorderLayout.PAGE_START);
-        this.add(klavesnica, BorderLayout.CENTER);
+        this.add(new MojPanel());
 
         this.setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println(e.getActionCommand());
     }
 }
