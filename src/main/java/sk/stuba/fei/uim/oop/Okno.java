3759f1b8e4104a1184b687fa4c02058fd38e5125
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-04-01 22:52:51 +0200

    adapter

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -1,31 +1,19 @@
 package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseEvent;
 
-public class Okno extends JFrame implements MouseListener, MouseMotionListener, KeyListener, ChangeListener {
+
+public class Okno extends UniversalAdapter {
     public Okno() throws HeadlessException {
         super();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400,400);
 
 
-        //this.addMouseListener(this);
-        //this.addMouseMotionListener(this);
-        //this.addKeyListener(this);
+        this.addMouseListener(this);
 
-        JSlider s = new JSlider(JSlider.HORIZONTAL,0,50,20);
-        s.setMinorTickSpacing(1);
-        s.setMajorTickSpacing(10);
-        s.setPaintTicks(true);
-        s.setPaintLabels(true);
-
-        s.addChangeListener(this);
-
-        this.add(s);
 
         this.setVisible(true);
     }
@@ -33,55 +21,5 @@ public class Okno extends JFrame implements MouseListener, MouseMotionListener, 
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("KLIK : " + e.getPoint());
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        System.out.println("PRESSED : " + e.getPoint());
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        System.out.println("RELEASED : " + e.getPoint());
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        System.out.println("ENTERED : " + e.getPoint());
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        System.out.println("EXITED : " + e.getPoint());
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        System.out.println("DRAGGED : " + e.getPoint());
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        System.out.println("MOVED : " + e.getPoint());
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println("TYPED : " + e.getKeyChar());
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        System.out.println("PRESSED : " + e.getKeyChar());
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        System.out.println("RELEASED : " + e.getKeyChar());
-    }
-
-    @Override
-    public void stateChanged(ChangeEvent e) {
-        System.out.println(((JSlider) e.getSource()).getValue());
     }
 }
